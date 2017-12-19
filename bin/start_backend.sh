bb93fc2f1eef7b67a89b16fdaa1573093d661cfc
Hui Wang <hedonplay@gmail.com>
2017-12-19 22:26:38 +0100

    update script

--- bin/start_backend.sh
+++ bin/start_backend.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 docker pull registry.gitlab.com/truelaurel/encore:master
-docker-compose -p encore -f docker-compose/docker-compose.yml up -d
+docker-compose -p encore -f docker-compose.yml up -d
