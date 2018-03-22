aeaaae02738abae33351a0893a1c4ed33413e534
hwang <hwang@murex.com>
2018-03-22 14:26:07 +0100

    add url in error message

--- src/main/java/com/truelaurel/encore/recommendation/DomainNameExtractor.java
+++ src/main/java/com/truelaurel/encore/recommendation/DomainNameExtractor.java
@@ -9,7 +9,7 @@ class DomainNameExtractor {
         try {
             return new URI(url).getHost();
         } catch (URISyntaxException e) {
-            throw new IllegalArgumentException("URL is illegal, unable to find domain name");
+            throw new IllegalArgumentException("URL is illegal, unable to find domain name " + url, e);
         }
     }
 }
